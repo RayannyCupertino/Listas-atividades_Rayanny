81196bda03f9b71127b49ec6cd7bf711a6d37106
RayannyCupertino <100451784+RayannyCupertino@users.noreply.github.com>
2022-03-21 22:30:14 -0300

    ui
    pacotes

---
+++ pacotesex/src/main/java/br/inatel/cdg/main.java
@@ -0,0 +1,16 @@
+package br.inatel.cdg;
+import br.inatel.cdg.matematica.matematica;
+
+public class main {
+
+    public static void main(String[] args) {
+
+       // br.inatel.cdg.matematica.matematica matematica= new br.inatel.cdg.matematica.matematica(){
+
+            matematica matematica = new matematica();
+
+        };
+
+
+    }
+}
